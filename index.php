89c9841fc5444ef63f2c939444435e0328d55374
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-03 17:59:45 +0800

    feat: inisialisasi struktur main HTML

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>User Login</title>
+  </head>
+  <body>
+    <h1>Selamat datang di Aplikasi Login</h1>
+    <a href="auth/form_login.php">Login</a>
+    <a href="auth/form_register.php">Register</a>
+  </body>
+</html>
